33b238fbe78f7e6f82a78d8d92255f01f3d53347
mohamedidbihi <medsougtani@gmail.com>
2022-01-27 11:09:16 +0100

    first

---
+++ src/main/java/com/bamco/bamcoreport/BamCoApplication.java
@@ -0,0 +1,12 @@
+package com.bamco.bamcoreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamCoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BamCoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamco/bamcoreport/BamCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamco.bamcoreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamCoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
